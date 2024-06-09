375540708a4800bdd85da70428e90a3cddfdb731
brian-ambani <brainambani1@gmail.com>
2024-06-09 09:17:32 +0300

    updated the folder structure

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Dashboard() {
+  return (
+    <main>
+      <p className="text-3xl text-green-500">Dashboard page(protected)</p>
+    </main>
+  );
+}
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <p className="text-3xl text-green-500">Landing page (Unprotected)</p>
+    </main>
+  );
+}
